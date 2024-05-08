6a5757c7cd82ee6ddfbbf972c4c24a60467859a2
Apisteftos <kosagglis@gmail.com>
2024-05-08 14:22:12 +0200

    update

--- run.sh
+++ run.sh
@@ -8,17 +8,18 @@ if [ "$#" -eq 2 ]; then
 
     arg1=$1
     arg2=$2
+    title="Bash Clocker"
     
     case $arg1 in 
         basic) 
             width=122
             height=19
-            gnome-terminal --geometry=${width}x${height} --profile="${arg2}" -- bash -c "bash bash_clock.sh ${arg1} Symbols/basic/colon.sh"
+            gnome-terminal --geometry=${width}x${height} --title="${title}" --profile="${arg2}" -- bash -c "bash bash_clock.sh ${arg1} Symbols/basic/colon.sh"
             ;;
         basic3d)
             width=140
             height=20
-            gnome-terminal --geometry=${width}x${height} --profile="${arg2}" -- bash -c "bash bash_clock.sh ${arg1} Symbols/basic3d/colon3d.sh"
+            gnome-terminal --geometry=${width}x${height} --title="${title}" --profile="${arg2}" -- bash -c "bash bash_clock.sh ${arg1} Symbols/basic3d/colon3d.sh"
             ;;
         *)
             echo "Error!" 
